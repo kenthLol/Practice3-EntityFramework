0bb4a8e332cebf075437db3fb0be631d08e6bf40
wegner01 <kenneth.lola119@est.unanleon.edu.ni>
2023-08-22 08:48:58 -0600

    Fixed user create controller

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -55,7 +55,7 @@ namespace thirdPractice.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Id,Name,LastName,Email,Birthday,Photo")] User user)
+        public async Task<IActionResult> Create([Bind("Id,FirstName,LastName,Email,Birthday,Photo")] User user)
         {
             if (ModelState.IsValid)
             {
@@ -87,7 +87,7 @@ namespace thirdPractice.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Name,LastName,Email,Birthday,Photo")] User user)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,FirstName,LastName,Email,Birthday,Photo")] User user)
         {
             if (id != user.Id)
             {
